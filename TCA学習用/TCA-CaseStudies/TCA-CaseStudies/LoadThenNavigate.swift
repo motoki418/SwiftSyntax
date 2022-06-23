65abfce81492c251855321e11fb215246c156df7
Motoki Nakamura <snkrs418@gmail.com>
2022-06-23 09:43:35 +0900

    Point-Freeの03-Navigation-LoadThenNavigateサンプルで、データをロードした後に画面遷移する方法について学習

---
+++ TCA学習用/TCA-CaseStudies/TCA-CaseStudies/LoadThenNavigate.swift
@@ -0,0 +1,20 @@
+//
+//  LoadThenNavigate.swift
+//  TCA-CaseStudies
+//
+//  Created by nakamura motoki on 2022/06/22.
+//
+
+import SwiftUI
+
+struct LoadThenNavigate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadThenNavigate_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadThenNavigate()
+    }
+}
